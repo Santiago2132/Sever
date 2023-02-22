c99f527dc074b11f25c2b10e35691e20a531d890
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-02-22 07:58:00 -0500

    Se removieron archivos

--- RMI/HelloService.java
+++
@@ -1,7 +0,0 @@
-package Sever.RMI;
-import java.rmi.RemoteException;
-
-public interface HelloService extends java.rmi.Remote
-{
-    String sayHello(String name) throws RemoteException;
-}
--- RMI/HelloServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package Sever.RMI;
-
-public class HelloServiceImpl
-{
-    public String sayHello(String name)
-    {
-        return "Hello, " + name;
-    }
-}
